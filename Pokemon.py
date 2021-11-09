c648d02d49c54c3034fddac63859643b2c33b280
Timofey Yatskov <90448236+cringecompiler@users.noreply.github.com>
2021-11-09 21:20:09 +0300

    Pokemon.py

---
+++ Pokemon.py
@@ -0,0 +1,87 @@
+import random
+from abc import abstractmethod
+
+
+class AnimeMon:
+    @classmethod
+    @abstractmethod
+    def inc_exp(cls, value):
+        pass
+
+    @property
+    @abstractmethod
+    def exp(cls):
+        pass
+
+    @exp.setter
+    @abstractmethod
+    def exp(self, value):
+        pass
+
+
+def train(animemon: AnimeMon):
+    step_size, level_size = 10, 100
+    sparring_qty = (level_size - animemon.exp % level_size) // step_size
+    for i in range(sparring_qty):
+        win = random.choice([True, False])
+        if win:
+            animemon.inc_exp(step_size)
+
+
+class BasePokemon:
+    def __str__(self):
+        return f'{self.name}/{self.poketype}'
+
+
+class EmojiMixin():
+    emoji = {'grass': '🌿', 'fire': '🔥', 'water': '🌊', 'electric': '⚡'}
+
+    def __str__(self):
+        text_poketype = super().__str__()
+        return text_poketype.replace(self.poketype, self.emoji[self.poketype])
+
+
+class Pokemon(EmojiMixin, BasePokemon, AnimeMon):
+    def __init__(self, name: str, poketype: str):
+        self.name = name
+        self.poketype = poketype
+        self._exp = 0
+
+    def inc_exp(self, value: int):
+        self._exp += value
+
+    @property
+    def exp(self):
+        return self._exp
+
+    @exp.setter
+    def exp(self, value):
+        self._exp = value
+
+
+class Digimon(AnimeMon):
+    def __init__(self, name):
+        self.name = name
+        self._exp = 0
+
+    def inc_exp(self, value: int):
+        self.exp += value * 8
+
+    @property
+    def exp(self):
+        return self._exp
+
+    @exp.setter
+    def exp(self, value):
+        self._exp = value
+
+
+if __name__ == '__main__':
+    agumon = Digimon(name='Agumon')
+    train(agumon)
+    print(f'{agumon.exp = }')
+
+    bulbasaur = Pokemon(name='Bulbasaur', poketype='grass')
+    train(bulbasaur)
+    print(f'{bulbasaur.exp = }')
+    print(bulbasaur)
